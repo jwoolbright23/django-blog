3178deee66f15b32ae8738c07d8762755e8b42f3
Your Name <you@example.com>
2019-05-07 15:10:03 -0500

    added bootstrap for homepage

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+
+#Create a Blog Model
+
+#Add the blog app to the settings
+
+# create a migration
+
+#Migrate changes
+
+# Add to the admin
