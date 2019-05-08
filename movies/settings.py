58f83f9ebb33aa0ec5a45c1d9e778e2d8fe6102b
Your Name <you@example.com>
2019-05-08 16:12:25 -0500

    minor settings change

--- movies/settings.py
+++ movies/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'moviesdb',
         'USER': 'postgres',
-        'PASSWORD': 'Jdw23rkw',
+        'PASSWORD': 'djangodb',
         'HOST': 'localhost',
         'PORT': '5432',
 
